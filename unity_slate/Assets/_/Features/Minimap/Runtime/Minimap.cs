41c827b547576f15b5512a7cd9d6cd9a9b1dce67
Ambre <bourdon_ambre@outlook.fr>
2025-11-06 14:05:21 +0100

    Minimap prefab

--- unity_slate/Assets/_/Features/Minimap/Runtime/Minimap.cs
+++ unity_slate/Assets/_/Features/Minimap/Runtime/Minimap.cs
@@ -5,16 +5,80 @@ namespace Minimap.Runtime
 {
     public class Minimap : FBehaviour
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-        
-        }
+        public Texture2D minimapTexture;
+        [SerializeField] Rect minimapRect = new Rect(0, 0, 200, 200);
+        [SerializeField] Color borderColor = Color.white;
 
-        // Update is called once per frame
+        [SerializeField] Vector2 playerPos = new Vector2(0.5f, 0.5f);
+        [SerializeField] Vector2 playerViewSize = new Vector2(0.2f, 0.2f);
+
+        public Transform m_camera_position;
+        [SerializeField] Vector2 worldMin = new Vector2(-50, -50);
+        [SerializeField] Vector2 worldMax = new Vector2(50, 50);
+
         void Update()
         {
-        
+            if (m_camera_position)
+            {
+                playerPos.x = Mathf.InverseLerp(worldMin.x, worldMax.x, m_camera_position.position.x);
+                playerPos.y = Mathf.InverseLerp(worldMin.y, worldMax.y, m_camera_position.position.y);
+            }
+        }
+
+        void OnGUI()
+        {
+            float w = 250, h = 250;
+            Rect mapRect = new Rect(Screen.width - w - 20, Screen.height - h - 20, w, h);
+
+            if (minimapTexture)
+                GUI.DrawTexture(mapRect, minimapTexture, ScaleMode.ScaleToFit);
+            else
+                GUI.Box(mapRect, "Minimap");
+
+            float innerW = mapRect.width * playerViewSize.x;
+            float innerH = mapRect.height * playerViewSize.y;
+
+            float px = mapRect.x + mapRect.width * playerPos.x - innerW / 2f;
+            float py = mapRect.y + mapRect.height * (1 - playerPos.y) - innerH / 2f;
+
+            Rect viewRect = new Rect(px, py, innerW, innerH);
+
+            DrawDashedRect(viewRect, borderColor, 8f, 4f, 2f);
+        }
+
+        void DrawDashedRect(Rect rect, Color color, float dashLength, float gap, float thickness)
+        {
+            Texture2D tex = Texture2D.whiteTexture;
+            GUI.color = color;
+
+            // Top
+            DrawDashedLine(rect.xMin, rect.yMin, rect.xMax, rect.yMin, dashLength, gap, thickness, tex);
+            // Bottom
+            DrawDashedLine(rect.xMin, rect.yMax, rect.xMax, rect.yMax, dashLength, gap, thickness, tex);
+            // Left
+            DrawDashedLine(rect.xMin, rect.yMin, rect.xMin, rect.yMax, dashLength, gap, thickness, tex);
+            // Right
+            DrawDashedLine(rect.xMax, rect.yMin, rect.xMax, rect.yMax, dashLength, gap, thickness, tex);
+        }
+
+        void DrawDashedLine(float x1, float y1, float x2, float y2, float dash, float gap, float thickness, Texture2D tex)
+        {
+            float dx = x2 - x1;
+            float dy = y2 - y1;
+            float length = Mathf.Sqrt(dx * dx + dy * dy);
+            float angle = Mathf.Atan2(dy, dx) * Mathf.Rad2Deg;
+
+            float drawLength = 0f;
+            while (drawLength < length)
+            {
+                float seg = Mathf.Min(dash, length - drawLength);
+                Matrix4x4 matrix = GUI.matrix;
+                GUIUtility.RotateAroundPivot(angle, new Vector2(x1 + dx * (drawLength / length), y1 + dy * (drawLength / length)));
+
+                GUI.DrawTexture(new Rect(x1 + dx * (drawLength / length), y1 + dy * (drawLength / length), seg, thickness), tex);
+                GUI.matrix = matrix;
+                drawLength += dash + gap;
+            }
         }
     }
 }
