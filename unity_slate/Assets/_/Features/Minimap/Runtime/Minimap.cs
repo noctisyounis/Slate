c247a2a228a1721d9e518e35d8f689063121e54d
Ambre <bourdon_ambre@outlook.fr>
2025-11-06 11:32:58 +0100

    Minimap logic

---
+++ unity_slate/Assets/_/Features/Minimap/Runtime/Minimap.cs
@@ -0,0 +1,20 @@
+using Foundation.Runtime;
+using UnityEngine;
+
+namespace Minimap.Runtime
+{
+    public class Minimap : FBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
