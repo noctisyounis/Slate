b6d11c9f28ab631ec49120e163e784a0bc1c02f8
Voyager-001 <artyom.nicolay@hotmail.com>
2025-11-17 16:40:39 +0100

    Add settings

---
+++ unity_slate/Assets/_/Features/Style/Runtime/Scripts/GlobalFontApplier.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UImGui;
+
+namespace Style.Runtime
+{
+    [DisallowMultipleComponent]
+    public class GlobalFontApplier : MonoBehaviour
+    {
+        #region Unity
+
+            public void OnEnable()  => UImGuiUtility.Layout += OnLayout;
+            public void OnDisable() => UImGuiUtility.Layout -= OnLayout;
+            
+        #endregion
+
+        #region Layout
+
+            private void OnLayout(UImGui.UImGui ui)
+            {
+                FontRegistry.ApplyAsDefault();
+            }
+
+        #endregion
+    }
+}
---
+++ unity_slate/Assets/_/Features/Style/Runtime/Scripts/SizeSettingsPanel.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Style.Runtime
+{
+    public class SizeSettingsPanel : MonoBehaviour
+    {
+        
+    }
+}
